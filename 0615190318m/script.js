5e8f8b837c2f776a7aa546997704029a8bc83c71
MishaSv <mihaosv@gmail.com>
2019-03-22 10:39:45 +0200

    first
    first

---
+++ 0615190318m/script.js
@@ -0,0 +1,58 @@
+let comments = [];
+loadComments();
+
+
+function checkForm(form){
+  if (document.getElementById("comment-body").value == '')
+{
+  alert ('Заполните все поля');
+  return false;
+}
+}
+document.getElementById('comment-add').onclick = function(){
+	event.preventDefault();
+	let commentName = document.getElementById('comment-name');
+	let commentBody = document.getElementById('comment-body');
+
+	let comment = {
+		name : 'Иммануил Кант',
+		body : commentBody.value,
+		time : Math.floor(Date.now()/1000)
+	}
+	checkForm();
+	commentName = '';
+	commentBody = '';
+	comments.push(comment);
+	saveComments();
+	showComments();
+}
+
+function saveComments() {
+	localStorage.setItem('comments', JSON.stringify(comments));
+}
+	
+function loadComments() {
+	if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+	showComments();
+}
+
+function showComments() {
+	let commentField = document.getElementById("comment-field");
+	let out = '';
+	comments.forEach(function(item) {
+		out += `<div class="review_name mt">${item.name}</div> `;
+		out += `<div class="review_date mt">${timeConverter(item.time)}</div> `;
+		out += `<div class="comment">${item.body}</div> `;
+	});
+	commentField.innerHTML = out;
+}
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = date + ' ' + month + ' ' + year;
+    return time;
+  }
